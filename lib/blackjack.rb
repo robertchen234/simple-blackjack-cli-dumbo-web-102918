23bf48049e43209c4dcd1336752bec6df5be2a50
Robert Chen <robertchen234@gmail.com>
2018-10-20 16:22:17 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,7 +32,7 @@ def hit?(card_total)
   prompt_user
   if get_user_input == 'h'
     card_total += deal_card
-  elsif get_user_input != "s"
+  elsif get_user_input == "s"
     card_total
   else
     invalid_command
